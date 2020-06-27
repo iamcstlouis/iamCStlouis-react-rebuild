d8b5e55501922b6f72582bc4423c0e17b23544b5
Chantal St Louis <misscstlouis@gmail.com>
2020-06-28 00:50:11 +0100

    Adds basic Header scaffolding and project images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 
-import './App.css';
 import './assets/scss/reset.scss'
 
 function App() {
   return (
-    <div className="App">
-      <Header></Header>
+    <div>
+      <Header />
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,112 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 import { Container, Col, Row } from 'react-bootstrap'
+
+import Logo from '../../images/global/cs-logo.png'
 
 const Header = () => {
     return (
         <header>
-            <Container >
+            <Container>
                 <Row>
-                    <Col xs={2} style={{ border: 'solid red 2px' }}>
-                        <h1>Hellloooooooooo</h1>
+                    <Col xs={12} style={{ border: 'solid red 2px' }}>
+                        <div className='logo'>
+                            <div className='logo-wrapper'>
+                                <Link to='/'>
+                                    <img
+                                        src={Logo}
+                                        className='logo'
+                                        alt='iamCStlouis Logo'
+                                    />
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className={`menu-toggler`}>
+                            <div className='hamburger-btn'>
+                                <div className={`bar bar__top}`}></div>
+                                <div className={`bar bar__mid}`}></div>
+                                <div className={`bar bar__btm}`}></div>
+                            </div>
+                        </div>
+
+                        <div id='mobile-menu' className={`mobile-menu`}>
+                            <div className='mobile-menu-links'>
+                                <div className='container'>
+                                    <ul>
+                                        <li>
+                                            <NavLink
+                                                exact
+                                                to='/'
+                                            >
+                                                Home
+                                        </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink
+                                                exact
+                                                to='/work'
+                                            >
+                                                Work
+                                        </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink
+                                                exact
+                                                to='/about'
+                                            >
+                                                About
+                                        </NavLink>
+                                        </li>
+                                        <li>
+                                            <NavLink
+                                                exact
+                                                to='contact'
+                                            >
+                                                Contact
+                                        </NavLink>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+
+                        {/* <div className='desktop-menu'>
+                            <div className='desktop-links'>
+                                <ul>
+                                    <li>
+                                        <NavLink
+                                            exact
+                                            to='/'>
+                                            Home
+                                </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            exact
+                                            to='/work'>
+                                            Work
+                                </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            exact
+                                            to='/about'>
+                                            About
+                                </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink
+                                            exact
+                                            to='contact'>
+                                            Contact
+                                </NavLink>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div> */}
                     </Col>
                 </Row>
-
             </Container>
         </header>
     )
