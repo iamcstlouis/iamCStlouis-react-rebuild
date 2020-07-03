6d4bfe5375394f37d19f07b6e7dee0f6be6ccb38
Chantal St Louis <misscstlouis@gmail.com>
2020-07-03 01:24:42 +0100

    Adds MainCopy component to Home

---
+++ src/pages/Home/MainCopy/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import './styles.scss'
+
+const MainCopy = () => {
+    return (
+        <div className='what-i-do-txt-wrapper'>
+            <p>
+                Welcome to iamCStlouis’ website. If you're looking for original, top quality paintings
+                and
+                prints,
+                then you have
+                come to the right place!
+            </p>
+
+            <p>
+                A lot of iamCStlouis’ work is commission based, so please feel free to contact her to discuss
+                your
+                requirements. Similarly,
+                if you have any enquiries about any of the paintings you see here, then once again please do not
+                hesitate to
+                get in
+                touch.
+                Thanks for stopping by…
+            </p>
+        </div>
+    )
+}
+
+export default MainCopy
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Hero from './Hero'
 import PageTitle from '../../components/PageTitle'
+import MainCopy from './MainCopy'
 
 const Home = () => {
     return (
         <React.Fragment>
             <Hero />
             <PageTitle title={'What I Do'} />
+            <MainCopy />
         </React.Fragment>
     )
 }
