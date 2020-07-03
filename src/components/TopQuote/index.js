8f81d1e152f3ed0be841492515dd21383b1792d6
Chantal St Louis <misscstlouis@gmail.com>
2020-07-03 17:09:31 +0100

    Adds Top Quote to About page

---
+++ src/components/TopQuote/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles.scss'
+
+const TopQuote = () => {
+    return (
+        <p className='top-quote'>
+            “Art <span>has</span> no limitations. <span>I simply allow my brush to</span> follow, <span>as my
+            mind</span> takes the lead.” <span className='artist-name'>- iamCStlouis
+            </span>
+        </p>
+    )
+}
+
+export default TopQuote
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PageTitle from '../../components/PageTitle'
+import TopQuote from '../../components/TopQuote'
 
 const About = () => {
     return (
         <React.Fragment>
             <PageTitle title={'About Me'} />
+            <TopQuote />
         </React.Fragment>
     )
 }
