6cb35864592dc917c71f159a10068fadce5468b7
Chantal St Louis <misscstlouis@gmail.com>
2020-07-08 00:36:34 +0100

    Removes TopSocials component from the DOM at mobile

--- src/components/ProfileAndContactInfo/index.js
+++ src/components/ProfileAndContactInfo/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Profile from '../Profile'
 import TopSocials from '../TopSocials'
 
@@ -11,14 +11,22 @@ const ProfileAndContactInfo = () => {
     // const [isHomepage, setProfile] = useState(pathname === '/')
     // const [isContactPage, setContactInfo] = useState(pathname === '/contact')
 
+    const [width, setWidth] = useState(window.innerWidth)
+    const breakpoint = 1024;
 
+    useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
 
     return (
         <React.Fragment>
 
             {pathname === '/' ? <Profile /> : pathname === '/contact' ? <ContactInfo /> : null}
 
-            <TopSocials />
+            {width >= breakpoint && <TopSocials />}
         </React.Fragment>
     )
 }
