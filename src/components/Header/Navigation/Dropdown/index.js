079df98a31d57c9d4d8ceddeae295510a7f1bdd5
Chantal St Louis <misscstlouis@gmail.com>
2020-07-07 20:02:30 +0100

    Adds functionality for 'click to close' desktop DropDown menu

--- src/components/Header/Navigation/Dropdown/index.js
+++ src/components/Header/Navigation/Dropdown/index.js
@@ -1,30 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavLink } from 'react-router-dom'
 
 import './styles.scss'
 
 const Dropdown = () => {
+    const [DdClosed, SetDdClosed] = useState(false);
+
+    function getDisplay() {
+        if (!DdClosed) {
+            return 'block'
+        } else {
+            return 'none'
+        }
+    }
 
     return (
-        <div className='dropdown'>
+        <div className='dropdown' style={{ display: getDisplay() }}>
             <ul className='dropdown__list'>
                 <li className='dropdown__list-item'>
                     <NavLink to='/portfolio/sheetMusic-paintings'
-                        className='dropdown__link'>
+                        className='dropdown__link'
+                        onClick={() => { SetDdClosed(!DdClosed) }}
+                    >
                         Sheet Music Paintings
                     </NavLink>
                 </li>
 
                 <li className='dropdown__list-item'>
                     <NavLink to='/portfolio/canvas-paintings'
-                        className='dropdown__link'>
+                        className='dropdown__link'
+                        onClick={() => { SetDdClosed(!DdClosed) }}>
                         Canvas Paintings
                     </NavLink>
                 </li>
 
                 <li className='dropdown__list-item'>
                     <NavLink to='/portfolio/commissioned-paintings'
-                        className='dropdown__link'>
+                        className='dropdown__link'
+                        onClick={() => { SetDdClosed(!DdClosed) }}>
                         Commissioned Paintings
                     </NavLink>
                 </li>
--- src/pages/Portfolio/MainContent/index.js
+++ src/pages/Portfolio/MainContent/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
-// import PageTitle from '../../../components/PageTitle'
 import SheetMusicGallery from './SheetMusicGallery'
 import CanvasGallery from './CanvasGallery'
 import CommissionedGallery from './CommissionedGallery'
