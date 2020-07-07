4e86c41fd92578e7df4ddd9fe94ec79dbb04146b
Chantal St Louis <misscstlouis@gmail.com>
2020-07-08 00:23:54 +0100

    Adds SubNav component to Portfolio page

---
+++ src/pages/Portfolio/MainContent/SubNav/index.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+
+import './styles.scss'
+
+const SubNav = () => {
+
+    return (
+        <div className="portfolio-subnav">
+            <ul className="portfolio-subnav__list">
+                <li className="portfolio-subnav__list-item">
+                    <NavLink to='/portfolio/sheetMusic-paintings' className="portfolio-subnav__link">Sheet Music</NavLink>
+                </li>
+                <li className="portfolio-subnav__list-item">
+                    <NavLink to='/portfolio/canvas-paintings' className="portfolio-subnav__link">Canvas</NavLink>
+                </li>
+                <li className="portfolio-subnav__list-item">
+                    <NavLink to='/portfolio/commissioned-paintings' className="portfolio-subnav__link">Commissioned</NavLink>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default SubNav
--- src/pages/Portfolio/MainContent/index.js
+++ src/pages/Portfolio/MainContent/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Route, Redirect } from 'react-router-dom'
 
 import SheetMusicGallery from './SheetMusicGallery'
@@ -6,10 +6,26 @@ import CanvasGallery from './CanvasGallery'
 import CommissionedGallery from './CommissionedGallery'
 
 import '../MainContent/styles.scss'
+import SubNav from './SubNav'
 
 const MainContent = () => {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const breakpoint = 1024;
+
+    useEffect(() => {
+        const handleWindowResize = () => setWidth(window.innerWidth);
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => window.removeEventListener('resize', handleWindowResize);
+    }, []);
+
     return (
         <React.Fragment>
+
+            {width < breakpoint && <SubNav />}
+
+
             <Route exact path='/portfolio' render={() => <Redirect to='/portfolio/sheetMusic-paintings' />} />
             <Route path='/portfolio/sheetMusic-paintings' component={SheetMusicGallery} />
             <Route path='/portfolio/canvas-paintings' component={CanvasGallery} />
