2530f6c145b3f5094128f38f35d48031599b776c
Chantal St Louis <misscstlouis@gmail.com>
2020-07-08 01:20:10 +0100

    Prints quotation marks to the DOM on Testimonies

--- src/pages/Testimonials/MainContent/TestimonialsData/index.js
+++ src/pages/Testimonials/MainContent/TestimonialsData/index.js
@@ -8,7 +8,7 @@ const TestimonialsData = [
     {
         img: ZalonImage,
         alt: 'Zalon Music with iamCStlouis Amy Winehouse painting',
-        testimonialQuote: 'It’s beautiful. Thank you for the beauty you add to this world.',
+        testimonialQuote: '"It’s beautiful. Thank you for the beauty you add to this world."',
         testimonyFrom: 'Zalon Music',
         excerpt: 'Musician and good friend of the late Amy Winehouse.',
         id: 'zalon-testimony'
@@ -16,7 +16,7 @@ const TestimonialsData = [
     {
         img: KojoImage,
         alt: 'iamCStlouis presenting her Kojo painting to Kojo the comedian',
-        testimonialQuote: 'I don’t like anything of myself. But this, this is sick!',
+        testimonialQuote: '"I don’t like anything of myself. But this, this is sick!"',
         testimonyFrom: 'Kojo The Comedian',
         excerpt: 'Comedian and former Capital Xtra presenter.',
         id: 'kojo-testimony'
@@ -24,7 +24,7 @@ const TestimonialsData = [
     {
         img: ZahaImage,
         alt: 'iamCStlouis presenting her Zaha painting to Wilfred Zaha',
-        testimonialQuote: 'Amazing!… This really is the best gift I’ve received in a long time.',
+        testimonialQuote: '"Amazing!… This really is the best gift I’ve received in a long time."',
         testimonyFrom: 'Wilfred Zaha (of Crystal Palace FC)',
         excerpt: 'Presented at his birthday party.',
         id: 'zaha-testimony'
@@ -32,7 +32,7 @@ const TestimonialsData = [
     {
         img: BolasieImage,
         alt: 'iamCStlouis presenting her Bolasie painting to Yannick bolasie',
-        testimonialQuote: 'Best painted picture I’ve seen of myself.',
+        testimonialQuote: '"Best painted picture I’ve seen of myself."',
         testimonyFrom: 'Yannick Bolasie',
         excerpt: '(of Everton FC).',
         id: 'bolasie-testimony'
@@ -40,7 +40,7 @@ const TestimonialsData = [
     {
         img: SlimShadyImage,
         alt: 'Mike with his birthday surprise painting of Eminem',
-        testimonialQuote: 'Thank you so much for the Eminem painting. So good! The boys spoiled me... Thanks again',
+        testimonialQuote: '"Thank you so much for the Eminem painting. So good! The boys spoiled me... Thanks again"',
         testimonyFrom: 'Commissioned painting',
         excerpt: 'Surprise birthday present.',
         id: 'slim-shady-testimony'
