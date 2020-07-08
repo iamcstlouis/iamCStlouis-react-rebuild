576f7d5e223127fa90fc68c0c5d0da5cd5c2c1e7
Chantal St Louis <misscstlouis@gmail.com>
2020-07-08 21:23:42 +0100

    Adjusts AOS animation on contact form

--- src/pages/Contact/MainContent/Form/index.js
+++ src/pages/Contact/MainContent/Form/index.js
@@ -11,21 +11,22 @@ const Form = () => {
                 data-netlify='true'
                 data-netlify-honeypot='bot-field'
                 className='contact-form'
+                data-aos='fade-up' data-aos-delay='900'
             >
 
                 <input type='hidden' name='contact-form' value='painting-enquiry' />
 
-                <h2 className='form-header' data-aos='fade-up' data-aos-delay='600'>General Enquiries</h2>
+                <h2 className='form-header'>General Enquiries</h2>
 
                 <input type='hidden' name='form-name' value='website-price-enquiry' />
-                <label data-aos='fade-up' data-aos-delay='700'>Name</label>
-                <input type='text' name='name' required data-aos='fade-up' data-aos-delay='900' />
-                <label data-aos='fade-up' data-aos-delay='1000'>Email</label>
-                <input type='text' name='email' required data-aos='fade-up' data-aos-delay='1200' />
-                <label data-aos='fade-up' data-aos-delay='1400'>Message</label>
-                <textarea name='message' required data-aos='fade-up' data-aos-delay='1600'></textarea>
+                <label>Name</label>
+                <input type='text' name='name' required />
+                <label>Email</label>
+                <input type='text' name='email' required />
+                <label>Message</label>
+                <textarea name='message' required></textarea>
 
-                <button type='submit' className='submit-btn' data-aos='fade-up' data-aos-delay='1800'>
+                <button type='submit' className='submit-btn'>
                     <span>Send</span>
                 </button>
             </form>
